fd1f1b5de03af2403eed262881a302004c26cdfc
aminiyolo <73641636+aminiyolo@users.noreply.github.com>
2021-04-23 14:34:36 +0900

    React-redux - Using redux-actions

--- react-withRedux/src/modules/counter.js
+++ react-withRedux/src/modules/counter.js
@@ -1,10 +1,12 @@
+import { createAction, handleActions } from "redux-actions";
+
 // Define action type
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // Create action creator
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // Default value
 const initialState = {
@@ -12,19 +14,12 @@ const initialState = {
 };
 
 // Reducer function
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
 
 export default counter;
